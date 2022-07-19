c78dcfb68670b7aae8f530cec4d3dbdfd1aa15f4
Clyde Lu <92775935+p42777@users.noreply.github.com>
2022-07-19 23:02:00 +0800

    Initial commit

---
+++ CPP/C0231.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Solution{
+    public:
+        bool isPowerOfTwo(int n){
+            
+            if (n == 0)
+                return false;
+            if (n == 1)
+                return true;
+            while (n != 1){
+                if (n % 2 != 0)
+                    return false;
+                n /= 2;
+            }
+            return true;
+        }
+        
+};
---
+++ Java/J0144.java
@@ -0,0 +1,39 @@
+package Java;
+import java.util.ArrayList;
+import java.util.List;
+
+public class J0144 {
+    
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+           this.val = val;
+           this.left = left;
+           this.right = right;
+        }
+    }
+    
+    class Solution {
+        public List<Integer> preorderTraversal(TreeNode root) {
+            List<Integer> result = new ArrayList<>();
+            depthFirstSearch(root, result);
+            return result;
+
+        }
+
+        public void depthFirstSearch(TreeNode root, List<Integer> result) {
+            if (root == null)
+                return;
+            result.add(root.val);
+            depthFirstSearch(root.left, result);
+            depthFirstSearch(root.right, result);
+
+        }
+    }
+}
